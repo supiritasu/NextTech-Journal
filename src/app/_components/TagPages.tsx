fd02c7ba0d5c7637554d7ab2ce6e77e9b07261fd
supiritasu <supiritasu.supiritasu@gmail.com>
2024-06-22 08:09:39 +0900

    header編集

--- src/app/_components/TagPages.tsx
+++ src/app/_components/TagPages.tsx
@@ -1,45 +1,47 @@
-// pages/tags/[tag].tsx
 import React from "react";
+import { GetStaticProps, GetStaticPaths } from "next";
+import Head from "next/head";
+import TagPage from "@/app/_components/TagPages";
 import { getAllTags, getPostsByTag } from "../../lib/api";
-import TagPage from "../_components/TagPages";
-
-type Post = {
-  slug: string;
-  title: string;
-  date: string;
-  tags: string[];
-};
+import { Post } from "@/interfaces/post";
 
 type Props = {
   posts: Post[];
   tag: string;
 };
 
+const Tag = ({ posts, tag }: Props) => {
+  return (
+    <>
+      <Head>
+        <title>Tag: {tag}</title>
+      </Head>
+      <TagPage posts={posts} tag={tag} />
+    </>
+  );
+};
 
-export default function Tag({ posts, tag }: Props) {
-  return <TagPage posts={posts} tag={tag} />;
-}
+export default Tag;
 
-export const getStaticProps = ({ params }: { params: { tag: string } }) => {
-  const posts = getPostsByTag(params.tag);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const tag = params?.tag as string;
+  const posts = getPostsByTag(tag);
 
   return {
     props: {
       posts,
-      tag: params.tag,
+      tag,
     },
   };
 };
 
-export function getStaticPaths() {
-    const tags = getAllTags();
-    return {
-      paths: tags.map((tag) => ({
-        params: {
-          tag: tag,
-        },
-      })),
-      fallback: false, 
-    };
-  }
-  
+export const getStaticPaths: GetStaticPaths = async () => {
+  const tags = getAllTags();
+
+  return {
+    paths: tags.map((tag) => ({
+      params: { tag },
+    })),
+    fallback: false,
+  };
+};
--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -11,16 +11,11 @@ const Header = () => {
         <span className="ml-3 text-xl">Tailblocks</span>
       </a>
       <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-        <a className="mr-5 hover:text-white">First Link</a>
-        <a className="mr-5 hover:text-white">Second Link</a>
-        <a className="mr-5 hover:text-white">Third Link</a>
-        <a className="mr-5 hover:text-white">Fourth Link</a>
+        <a className="mr-5 hover:text-white">About</a>
+        <a className="mr-5 hover:text-white">Blog</a>
+        <a className="mr-5 hover:text-white">Contact</a>
       </nav>
-      <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
-        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-          <path d="M5 12h14M12 5l7 7-7 7"></path>
-        </svg>
-      </button>
+      
     </div>
   </header>
   );
