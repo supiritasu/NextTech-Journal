7a6777af64e2275c035546f6da18df336b7eae04
supiritasu <supiritasu.supiritasu@gmail.com>
2024-06-22 09:25:24 +0900

    contactの作成

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -4,17 +4,24 @@ const Header = () => {
   return (
     <header className="text-gray-400 bg-gray-900 body-font">
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
-          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-        </svg>
-        <span className="ml-3 text-xl">Tailblocks</span>
-      </a>
-      <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-        <a className="mr-5 hover:text-white">About</a>
-        <a className="mr-5 hover:text-white">Blog</a>
-        <a className="mr-5 hover:text-white">Contact</a>
-      </nav>
+    <Link href="/" className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
+    <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+  </svg>
+  <span className="ml-3 text-xl">Tailblocks</span>
+</Link>
+
+<nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
+  <Link href="/about" className="mr-5 hover:text-white">
+    About
+  </Link>
+  <Link href="/blog" className="mr-5 hover:text-white">
+    Blog
+  </Link>
+  <Link href="/contact" className="mr-5 hover:text-white">
+    Contact
+  </Link>
+</nav>
       
     </div>
   </header>
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,21 @@
+import Container from "@/app/_components/container";
+import { HeroPost } from "@/app/_components/hero-post";
+import { Intro } from "@/app/_components/intro";
+import { MoreStories } from "@/app/_components/more-stories";
+import { getAllPosts } from "@/lib/api";
+import { ContainerWithChildren } from "postcss/lib/container";
+import { Contact } from "@/app/_components/contact";
+import Header from "../_components/header";
+
+
+export default function Index() {
+  
+  return (
+    <>
+    <Header/>
+    <main>
+      <Contact />
+    </main>
+    </>
+  );
+}
