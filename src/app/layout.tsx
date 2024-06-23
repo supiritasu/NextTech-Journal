7e5e2e66796e3310f5fcb73f6daf78b55cde7fa2
supiritasu <supiritasu.supiritasu@gmail.com>
2024-06-24 02:03:47 +0900

    change-tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,8 +77,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Footer />
         <Analytics/>
       </body>
-      <GoogleTagManager gtmId="G-KMQDB224Q6"/>
-      <GoogleAnalytics gaId="G-KMQDB224Q6" />
+      <GoogleTagManager gtmId="G-50Z92B2D6S"/>
+      <GoogleAnalytics gaId="G-50Z92B2D6S" />
     </html>
   );
 }
