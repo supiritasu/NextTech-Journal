f72755a269988776c96180fb2fd7ce8a0610291b
supiritasu <supiritasu.supiritasu@gmail.com>
2024-06-25 07:34:33 +0900

    keyword

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: 'NextTechへようこそ！プログラミング、旅行、留学、雑談の記事を投稿',
   description: 'NextTechでは、一般大学生がプログラミング、旅行、留学、雑談に関する記事を不定期に投稿しています。プログラミングの解説、留学体験記、おすすめの旅行先、実用的な雑談記事などをご覧いただけます。',
-  keywords: 'プログラミング,旅行,留学,雑談,大学生ブログ',
+  keywords: 'プログラミング,旅行,留学,雑談,大学生ブログ,React,Next.js,TypeSciprt',
   authors: [{ name: 'NextTech' }],
   openGraph: {
     title: 'NextTech - プログラミング、旅行、留学、雑談の記事',
