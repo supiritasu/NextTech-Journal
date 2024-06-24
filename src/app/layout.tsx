64ac86ff5740e1ef5a55abdf79e109c0ab3cef52
supiritasu <supiritasu.supiritasu@gmail.com>
2024-06-24 13:54:19 +0900

    GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,19 +61,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ja">
       <head>
       <meta name="google-adsense-account" content="ca-pub-3517714019756094"/>
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer', 'GTM-M26PC82Q');
-            `,
-          }}
-        />
+
+      <GoogleTagManager gtmId="GTM-M26PC82Q" />
+      
       </head>
       <body className={inter.className}>
         <noscript>
@@ -88,7 +78,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <div className="min-h-screen">{children}</div>
         <Footer />
       </body>
-      <GoogleAnalytics gaId="G-KMQDB224Q6" />
+      <GoogleAnalytics gaId="G-50Z92B2D6S" />
     </html>
   );
 }
