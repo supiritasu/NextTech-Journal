5092be189a3d61a35c3b7b1852dedc0880b0fc32
supiritasu <supiritasu.supiritasu@gmail.com>
2024-06-24 11:18:27 +0900

    ch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import Header from './_components/header';
 import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google'
 import { Metadata } from 'next';
 
+import Script from 'next/script'
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -54,19 +56,39 @@ export const metadata: Metadata = {
   },
 };
 
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
       <head>
-
-      <GoogleAnalytics gaId="G-KMQDB224Q6" />
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', 'GTM-M26PC82Q');
+            `,
+          }}
+        />
       </head>
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M26PC82Q"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Header/>
         <div className="min-h-screen">{children}</div>
         <Footer />
       </body>
-      <GoogleTagManager gtmId="GTM-M26PC82Q" />
+      <GoogleAnalytics gaId="G-KMQDB224Q6" />
     </html>
   );
 }
