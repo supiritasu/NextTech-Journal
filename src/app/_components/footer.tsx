710bc3552dde0b950fd3389789d96681234c0aae
supiritasu <supiritasu.supiritasu@gmail.com>
2024-07-14 16:23:09 +0900

    favicon

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,14 +1,21 @@
 import Container from "@/app/_components/container";
 import { EXAMPLE_PATH } from "@/lib/constants";
+import Image from 'next/image';
 
 export function Footer() {
   return (
     <footer className="text-gray-400 bg-gray-900 body-font border-t border-gray-800">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
+          {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
+          </svg> */}
+            <Image 
+            src="favicon/NextTech_Icon.svg"
+            width={80}
+            height={80}
+            alt="SVG Icon"
+            />
           <span className="ml-3 text-xl">NextTech</span>
         </a>
         <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2024 NextTech —
